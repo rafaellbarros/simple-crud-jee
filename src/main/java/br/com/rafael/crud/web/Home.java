62994519a88a1bc5e35f5aa993fa21f52129e37a
Rafael Barros <rafaelbarros.df@gmail.com>
2016-02-20 23:47:50 -0200

    Criando servlet home

---
+++ src/main/java/br/com/rafael/crud/web/Home.java
@@ -0,0 +1,18 @@
+package br.com.rafael.crud.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/home")
+public class Home extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
+	}
+}
